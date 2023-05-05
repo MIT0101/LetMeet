c56b3cc3d3054bc40aa735188b0700f8e6a40eea
M_IT_01 <alraqym050@gmail.com>
2023-05-06 01:10:12 +0300

    create ui for genral reports,student reports ,ui only

--- LetMeet/Controllers/ReportController.cs
+++ LetMeet/Controllers/ReportController.cs
@@ -6,6 +6,8 @@ namespace LetMeet.Web.Controllers
     [Authorize(Roles ="Admin")]
     public class ReportController : Controller
     {
+        [HttpGet("/[Controller]/Admin")]
+        [Authorize(Roles ="Admin")]
         public IActionResult Index()
         {
             return View();
